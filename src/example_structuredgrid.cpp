6c065e450e6106cb6c7eff64b1ace34e3ef8d4d4
eugene <eugene.unn@gmail.com>
2018-02-26 19:36:45 +0300

    Add structured grid example

---
+++ src/example_structuredgrid.cpp
@@ -0,0 +1,55 @@
+#include "vtkActor.h"
+#include "vtkDataSetMapper.h"
+#include "vtkPoints.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkSmartPointer.h"
+#include "vtkStructuredGrid.h"
+#include "vtkStructuredPoints.h"
+
+int main()
+{
+
+	vtkSmartPointer<vtkPoints> P = 
+		vtkSmartPointer<vtkPoints>::New();
+	P->InsertNextPoint(0.0, 0.0, 0.0);
+	P->InsertNextPoint(1, 0, 0);
+	P->InsertNextPoint(0, 1, 0);
+	P->InsertNextPoint(1, 1, 0);
+	P->InsertNextPoint(0, 0, 1);
+	P->InsertNextPoint(1, 0, 1);
+	P->InsertNextPoint(0, 1, 1.5);
+	P->InsertNextPoint(1, 1, 2);
+
+
+	vtkSmartPointer<vtkStructuredGrid> SG = 
+		vtkSmartPointer<vtkStructuredGrid>::New();
+	SG->SetDimensions(2, 2, 2);
+	SG->SetPoints(P);
+
+	vtkSmartPointer<vtkDataSetMapper> mapper =
+		vtkSmartPointer<vtkDataSetMapper>::New();
+	mapper->SetInputData(SG);
+
+	vtkSmartPointer<vtkActor> actor =
+		vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+
+	vtkSmartPointer<vtkRenderer> renderer =
+		vtkSmartPointer<vtkRenderer>::New();
+	renderer->AddActor(actor);
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow =
+		vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactor->SetRenderWindow(renderWindow);
+
+	renderWindow->Render();
+	interactor->Start();
+
+	return 0;
+}
