671eb03fcef0db49e008f983cdd9038592f8888f
eugene <eugene.unn@gmail.com>
2018-02-27 00:50:47 +0300

    Add lightning example

---
+++ src/example_conelight.cpp
@@ -0,0 +1,59 @@
+#include <vtkConeSource.h>
+#include <vtkLight.h>
+#include <vtkPolyData.h>
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+
+int main()
+{
+
+	vtkSmartPointer<vtkConeSource> coneSource =
+		vtkSmartPointer<vtkConeSource>::New();
+	coneSource->SetResolution(100);
+	coneSource->Update();
+
+	vtkSmartPointer<vtkPolyDataMapper> mapper =
+		vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputConnection(coneSource->GetOutputPort());
+
+	vtkSmartPointer<vtkActor> actor =
+		vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+
+	/* Light */
+
+	vtkSmartPointer<vtkLight> light =
+		vtkSmartPointer<vtkLight>::New();
+	light->SetColor(1.0, 1.0, 0.0);
+	light->SetPosition(2.0, 1.0, 0.0);
+
+	vtkSmartPointer<vtkLight> light2 =
+		vtkSmartPointer<vtkLight>::New();
+	light2->SetColor(1.0, 0.0, 0.0);
+	light2->SetPosition(2.0, -1.0, 0.0);
+
+	/* end light */
+
+	vtkSmartPointer<vtkRenderer> renderer =
+		vtkSmartPointer<vtkRenderer>::New();
+	renderer->AddActor(actor);
+	renderer->AddLight(light);
+	renderer->AddLight(light2);
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow = 
+		vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor = 
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactor->SetRenderWindow(renderWindow);
+
+	renderWindow->Render();
+	interactor->Start();
+
+	return 0;
+}
