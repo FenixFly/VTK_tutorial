8150b778f872bfa83a572ddeb1e62574ffb556db
eugene <eugene.unn@gmail.com>
2018-02-27 01:08:03 +0300

    Add interaction example

---
+++ src/example_trackballcamera.cpp
@@ -0,0 +1,64 @@
+#include <vtkSmartPointer.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkPolyData.h>
+#include <vtkSphereSource.h>
+#include <vtkConeSource.h>
+#include <vtkInteractorStyleTrackballCamera.h>
+
+int main(int, char *[])
+{
+	vtkSmartPointer<vtkSphereSource> sphereSource =
+		vtkSmartPointer<vtkSphereSource>::New();
+	sphereSource->SetCenter(1.0, 0.0, 0.0);
+	sphereSource->Update();
+
+	vtkSmartPointer<vtkPolyDataMapper> sphereMapper =
+		vtkSmartPointer<vtkPolyDataMapper>::New();
+	sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+
+	vtkSmartPointer<vtkActor> sphereActor =
+		vtkSmartPointer<vtkActor>::New();
+	sphereActor->SetMapper(sphereMapper);
+
+	vtkSmartPointer<vtkConeSource> coneSource =
+		vtkSmartPointer<vtkConeSource>::New();
+
+	vtkSmartPointer<vtkPolyDataMapper> coneMapper =
+		vtkSmartPointer<vtkPolyDataMapper>::New();
+	coneMapper->SetInputConnection(coneSource->GetOutputPort());
+
+	vtkSmartPointer<vtkActor> coneActor =
+		vtkSmartPointer<vtkActor>::New();
+	coneActor->SetMapper(coneMapper);
+
+	vtkSmartPointer<vtkRenderer> renderer =
+		vtkSmartPointer<vtkRenderer>::New();
+	vtkSmartPointer<vtkRenderWindow> renderWindow =
+		vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+
+	vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	renderWindowInteractor->SetRenderWindow(renderWindow);
+
+	renderer->AddActor(sphereActor);
+	renderer->AddActor(coneActor);
+
+	/* Setup interaction style */
+
+	vtkSmartPointer<vtkInteractorStyleTrackballCamera> style =
+		vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New(); 
+
+	renderWindowInteractor->SetInteractorStyle(style);
+
+	/* end interaction setup */
+
+	renderWindow->Render();
+	renderWindowInteractor->Start();
+
+	return 0;
+}
