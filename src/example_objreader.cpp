0eb6f74e4f3dbd553daef6f38f5abc87dff5b407
eugene <eugene.unn@gmail.com>
2018-02-27 12:54:25 +0300

    Add example objReader

---
+++ src/example_objreader.cpp
@@ -0,0 +1,43 @@
+#include "vtkActor.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkSmartPointer.h"
+#include "vtkOBJReader.h"
+
+int main(int argc, char** argv)
+{
+	if (argc < 2)
+		return 1;
+
+	vtkSmartPointer<vtkOBJReader> reader =
+		vtkSmartPointer<vtkOBJReader>::New();
+	reader->SetFileName(argv[1]);
+	reader->Update();
+
+	vtkSmartPointer<vtkPolyDataMapper> mapper = 
+		vtkSmartPointer<vtkPolyDataMapper>::New();
+	mapper->SetInputConnection(reader->GetOutputPort());
+
+	vtkSmartPointer<vtkActor> actor = 
+		vtkSmartPointer<vtkActor>::New();
+	actor->SetMapper(mapper);
+
+	vtkSmartPointer<vtkRenderer> renderer = 
+		vtkSmartPointer<vtkRenderer>::New();
+	renderer->AddActor(actor);
+
+	vtkSmartPointer<vtkRenderWindow> renderWindow = 
+		vtkSmartPointer<vtkRenderWindow>::New();
+	renderWindow->AddRenderer(renderer);
+
+	vtkSmartPointer<vtkRenderWindowInteractor> interactor = 
+		vtkSmartPointer<vtkRenderWindowInteractor>::New();
+	interactor->SetRenderWindow(renderWindow);
+
+	renderWindow->Render();
+	interactor->Start();
+
+	return 0;
+}
